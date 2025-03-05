fa0445f5a0bfada3499a177771fcaaffd591c77f
fmfahath <fmfahath.dev@gmail.com>
2025-03-06 01:20:27 +0530

    client >> JobListing -- search by category section created, css added

--- client/src/components/JobListing.jsx
+++ client/src/components/JobListing.jsx
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react'
 import { AppContext } from '../context/AppContext'
-import { assets } from '../assets/assets';
+import { assets, JobCategories } from '../assets/assets';
 
 const JobListing = () => {
 
@@ -36,6 +36,19 @@ const JobListing = () => {
                         </>
                     )
                 }
+
+                {/* category filter */}
+                <div className='max-lg:hidden '>
+                    <h4 className='font-medium text-lg py-4'>Search by Category</h4>
+                    <ul className='space-y-4 text-gray-600'>
+                        {JobCategories.map((category, index) => (
+                            <li className='flex gap-3 items-center' key={index}>
+                                <input className='scale-125' type="checkbox" name='' id='' />
+                                {category}
+                            </li>
+                        ))}
+                    </ul>
+                </div>
             </div>
         </div>
     )
