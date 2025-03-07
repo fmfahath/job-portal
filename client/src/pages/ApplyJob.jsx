3146cb55362fb10ba7f4f17b9b7c88e171e54a97
fmfahath <fmfahath.dev@gmail.com>
2025-03-07 14:55:04 +0530

    client >> ApplyJob -- job right side section added

--- client/src/pages/ApplyJob.jsx
+++ client/src/pages/ApplyJob.jsx
@@ -76,7 +76,15 @@ const ApplyJob = () => {
                             <button className='bg-blue-600 p-2.5 px-10 text-white rounded cursor-pointer mt-10'>Apply Now</button>
                         </div>
 
-
+                        {/* right section */}
+                        <div className='w-full lg:w-1/3 mt-8 lg:mt-0 lg:ml-8 space-y-5'>
+                            <h2>More Jobs from {jobData.companyId.name}</h2>
+                            {jobs.filter(job => job._id !== jobData._id && job.companyId._id === jobData.companyId._id)
+                                .filter(job => true).slice(0, 4)
+                                .map((job, index) => (
+                                    <JobCard key={index} job={job} />
+                                ))}
+                        </div>
                     </div>
                 </div>
             </div>
