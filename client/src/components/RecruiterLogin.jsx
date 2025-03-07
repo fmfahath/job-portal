1eb561a9d6a1fe3805e66f2f0fe891269a721fa0
fmfahath <fmfahath.dev@gmail.com>
2025-03-07 21:34:10 +0530

    client >> Recruiter Login -- isDataSubmitted sate conditional rendering - company logo upload section created

--- client/src/components/RecruiterLogin.jsx
+++ client/src/components/RecruiterLogin.jsx
@@ -10,32 +10,53 @@ const RecruiterLogin = () => {
     const [image, setImage] = useState(false)
     const [isTextDataSubmitted, setIsTextDataSubmitted] = useState(false)
 
+    const onSubmitHandler = async (e) => {
+        e.preventDefault();
+
+        if (state === 'Sign Up' && !isTextDataSubmitted) {
+            setIsTextDataSubmitted(true)
+        }
+    }
+
 
     return (
         <div className='absolute top-0 left-0 right-0 bottom-0 z-10 flex justify-center items-center backdrop-blur-sm bg-black/30'>
-            <form className="relative bg-white p-10 rounded-xl text-slate-500">
+            <form className="relative bg-white p-10 rounded-xl text-slate-500" onSubmit={onSubmitHandler}>
                 <h1 className='text-center text-2xl text-neutral-700 font-medium '>Recruiter {state}</h1>
                 <p className='text-sm'>Welcome back! Please sign in to continue</p>
-                <>
-                    {state !== 'Login' && (
+                {state === 'Sign Up' && isTextDataSubmitted ?
+                    <>
+                        <div className='flex items-center gap-4 my-10'>
+                            <label htmlFor="image">
+                                <img className='w-16 rounded-full cursor-pointer' src={assets.upload_area} alt="" />
+                                <input type="file" id='image' hidden />
+                            </label>
+                            <p>Upload Company <br />Logo</p>
+                        </div>
+                    </>
+                    :
+                    <>
+                        {state !== 'Login' && (
+                            <div className='border px-4 py-2 flex items-center gap-2 rounded-full mt-5'>
+                                <img src={assets.person_icon} alt="" />
+                                <input className='outline-none text-sm' type="text" placeholder='Company Name' required onChange={e => setName(e.target.value)} value={name} />
+                            </div>
+                        )}
+                        <div className='border px-4 py-2 flex items-center gap-2 rounded-full mt-5'>
+                            <img src={assets.email_icon} alt="" />
+                            <input className='outline-none text-sm' type="email" placeholder='Email' required onChange={e => setEmail(e.target.value)} value={email} />
+                        </div>
                         <div className='border px-4 py-2 flex items-center gap-2 rounded-full mt-5'>
                             <img src={assets.person_icon} alt="" />
-                            <input className='outline-none text-sm' type="text" placeholder='Company Name' required onChange={e => setName(e.target.value)} value={name} />
+                            <input className='outline-none text-sm' type="password" placeholder='Password' required onChange={e => setPassword(e.target.value)} value={password} />
                         </div>
-                    )}
-                    <div className='border px-4 py-2 flex items-center gap-2 rounded-full mt-5'>
-                        <img src={assets.email_icon} alt="" />
-                        <input className='outline-none text-sm' type="email" placeholder='Email' required onChange={e => setEmail(e.target.value)} value={email} />
-                    </div>
-                    <div className='border px-4 py-2 flex items-center gap-2 rounded-full mt-5'>
-                        <img src={assets.person_icon} alt="" />
-                        <input className='outline-none text-sm' type="password" placeholder='Password' required onChange={e => setPassword(e.target.value)} value={password} />
-                    </div>
+                    </>
+                }
 
-                    <p className='text-sm text-blue-600 my-4 cursor-pointer'>Forgot password?</p>
-                </>
-                <button className='bg-blue-600 w-full text-white py-2 mb-2 rounded-full cursor-pointer'>
-                    {state === 'Login' ? 'Login' : 'Create Account'}
+                <p className='text-sm text-blue-600 my-4 cursor-pointer'>Forgot password?</p>
+
+                <button type='submit' className='bg-blue-600 w-full text-white py-2 mb-2 rounded-full cursor-pointer'>
+                    {state === 'Login' ? 'Login' : isTextDataSubmitted ? 'Create Account' : 'Next'}
                 </button>
 
                 {state === 'Login' ?
