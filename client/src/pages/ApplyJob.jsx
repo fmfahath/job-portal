b0dca188006233303c834b3c2db23cbd4f4545b0
fmfahath <fmfahath.dev@gmail.com>
2025-03-07 10:11:41 +0530

    client >> ApplyJob -- fetch jobData func added, Loading.jsx animation comp created

--- client/src/pages/ApplyJob.jsx
+++ client/src/pages/ApplyJob.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { AppContext } from '../context/AppContext'
+import Loading from '../components/Loading'
 
 const ApplyJob = () => {
-    return (
-        <div>ApplyJob</div>
-    )
+
+    const { id } = useParams()
+    const [jobData, setJobData] = useState(null)
+    const { jobs } = useContext(AppContext)
+
+    const fetchJob = async () => {
+        const data = jobs.filter(job => job._id === id)
+        if (data.length !== 0) {
+            setJobData(data[0])
+            // console.log(data[0]);
+        }
+    }
+
+    useEffect(() => {
+        if (jobs.length > 0) {
+            fetchJob()
+        }
+
+        // fetchJob()
+    }, [id, jobs])
+
+    return jobData ? (
+        <div>
+
+        </div>
+    ) :
+        (
+            <Loading />
+        )
 }
 
 export default ApplyJob
