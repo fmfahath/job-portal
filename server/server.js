97809db52db4c32b8cd7ba0116ade8505fa0db46
fmfahath <fmfahath.dev@gmail.com>
2025-03-09 07:08:23 +0530

    server >> sentry -- sentry account created, SDK addeded in project, instrument.js created in config folder, error handler added in server.js, API testet with sentry success

--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
+import './config/instrument.js'
 import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
 import connectDB from './config/db.js';
+import * as Sentry from "@sentry/node"
 
 //initialize express
 const app = express();
@@ -16,10 +18,16 @@ app.use(express.json())
 
 //routes
 app.get('/', (req, res) => res.send('API working...'))
+app.get("/debug-sentry", function mainHandler(req, res) {
+    throw new Error("My first Sentry error!");
+});
 
 
 //PORT
 const PORT = process.env.PORT || 4000
+
+//sentry
+Sentry.setupExpressErrorHandler(app);
 
 app.listen(PORT, () => {
     console.log(`server running on port:${PORT}`);
