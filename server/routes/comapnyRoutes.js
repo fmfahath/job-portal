099e4100c72dfbbb6a8569799e5b343292584c67
fmfahath <fmfahath.dev@gmail.com>
2025-03-11 17:01:34 +0530

    server >> companyController -- jobsModel.js, authMiddleware.js created, authMiddleware added in companyRoutes

--- server/routes/comapnyRoutes.js
+++ server/routes/comapnyRoutes.js
@@ -1,16 +1,17 @@
 import express from 'express'
 import { changeJobApplicationsStatus, changeJobVisibility, getCompanyData, getCompanyJobApplicants, getCompanyPostedJobs, loginCompany, postJob, registerCompany } from '../controllers/companyController.js'
 import upload from '../middlewares/multer.js'
+import protectCompany from '../middlewares/authMiddleware.js'
 
 const comapnyRouter = express.Router()
 
 comapnyRouter.post('/register', upload.single('image'), registerCompany)
 comapnyRouter.post('/login', loginCompany)
-comapnyRouter.get('/company', getCompanyData)
-comapnyRouter.post('/post-job', postJob)
-comapnyRouter.get('/list-jobs', getCompanyPostedJobs)
-comapnyRouter.get('/applicants', getCompanyJobApplicants)
-comapnyRouter.post('/change-status', changeJobApplicationsStatus)
+comapnyRouter.get('/company', protectCompany, getCompanyData)
+comapnyRouter.post('/post-job', protectCompany, postJob)
+comapnyRouter.get('/list-jobs', protectCompany, getCompanyPostedJobs)
+comapnyRouter.get('/applicants', protectCompany, getCompanyJobApplicants)
+comapnyRouter.post('/change-status', protectCompany, changeJobApplicationsStatus)
 comapnyRouter.post('/change-visibility', changeJobVisibility)
 
 export default comapnyRouter;
