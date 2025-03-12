91d21f50d9e0902d2137507720dc72b6255a19ff
fmfahath <fmfahath.dev@gmail.com>
2025-03-12 23:31:15 +0530

    server >> usersController -- getUserData handler crated

--- server/controllers/usersController.js
+++ server/controllers/usersController.js
@@ -1,7 +1,23 @@
+import userModel from "../models/userModel.js"
 
 
 //get user data
 export const getUserData = async (req, res) => {
+
+    const userId = req.auth.userId
+
+    try {
+        const user = await userModel.findById(userId)
+
+        if (!user) {
+            return res.status(404).json({ success: false, message: "User not found" })
+        }
+
+        res.status(200).json({ success: true, user })
+
+    } catch (error) {
+        res.status(500).json({ success: false, message: error.message })
+    }
 
 }
 
