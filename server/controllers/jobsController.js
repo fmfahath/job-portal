fbd2857f0d8b4f75d86f078e9b637ceb3d111e51
fmfahath <fmfahath.dev@gmail.com>
2025-03-12 06:31:24 +0530

    server >> jobsController -- getJobById handler created, API tested success

--- server/controllers/jobsController.js
+++ server/controllers/jobsController.js
@@ -16,5 +16,18 @@ export const getJobs = async (req, res) => {
 
 //get job by ID
 export const getJobbyId = async (req, res) => {
+    try {
+        const { id } = req.params;
 
+        const job = await jobsModel.findById(id).populate({ path: 'companyId', select: '-password' })
+
+        if (!job) {
+            return res.status(404).json({ success: false, message: 'Jobnot found' })
+        }
+
+        res.status(200).json({ success: true, job })
+
+    } catch (error) {
+        res.status(500).json({ success: false, message: error.message })
+    }
 }
