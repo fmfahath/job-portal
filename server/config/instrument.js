fc7dbb684296750d91c291758639b864fcac4083
fmfahath <fmfahath.dev@gmail.com>
2025-03-16 12:16:49 +0530

    Removed .env files from tracking

--- server/config/instrument.js
+++
@@ -1,8 +0,0 @@
-import * as Sentry from "@sentry/node"
-
-Sentry.init({
-    dsn: "https://9f635d5c11678b800386bb89900ed8ee@o4508943017639936.ingest.us.sentry.io/4508945142710272",
-    integrations: [
-        Sentry.mongooseIntegration()
-    ],
-});
--- server/routes/jobsRoute.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-import { getJobbyId, getJobs } from '../controllers/jobsController.js';
-
-const jobsRouter = express.Router()
-
-jobsRouter.get('/', getJobs)
-jobsRouter.get('/:id', getJobbyId)
-
-export default jobsRouter;
